9283f3e5705a0b49629cff78b53040c01862ef9d
Roman Blinkov <roman@blinkov.com>
2020-03-05 00:33:48 +0300

    [+] Fix unicode issue

--- CloudIpspSDK/Utils/Signature.cs
+++ CloudIpspSDK/Utils/Signature.cs
@@ -42,7 +42,7 @@ namespace CloudIpspSDK.Utils
         /// <returns></returns>
         private static string GetSha1(string value)
         {
-            var data = Encoding.ASCII.GetBytes(value);
+            var data = Encoding.UTF8.GetBytes(value);
             var hashData = new SHA1Managed().ComputeHash(data);
             var hash = string.Empty;
             foreach (var b in hashData)
